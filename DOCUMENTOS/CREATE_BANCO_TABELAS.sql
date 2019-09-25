a77d9fc9efc2ac7c11abb4e5057a1be87f678505
Vinicius Vieira <v.vieira.go@gmail.com>
2019-09-24 21:51:05 -0300

    Tioi alteração: Commit do SQL de criação da tabela

---
+++ DOCUMENTOS/CREATE_BANCO_TABELAS.sql
@@ -0,0 +1,10 @@
+-- --------------------------------------------------------
+-- Servidor:                     127.0.0.1
+-- Versão do servidor:           10.3.16-MariaDB - mariadb.org binary distribution
+-- OS do Servidor:               Win64
+-- HeidiSQL Versão:              10.2.0.5599
+-- --------------------------------------------------------
+
+CREATE DATABASE IF NOT EXISTS `bd_aviacao`;
+USE `bd_aviacao`;
+
