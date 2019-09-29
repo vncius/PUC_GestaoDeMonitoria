ff7f7548da71449f3c981c4c55a41ee08a792ed6
Vinicius Vieira <v.vieira.go@gmail.com>
2019-09-29 17:11:24 -0300

    Tipo de alteração: Implementado parte da estrutura de login.

---
+++ PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/controller/ControllerIndex.java
@@ -0,0 +1,43 @@
+package com.monitoria.puc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SuppressWarnings("rawtypes")
+@RestController
+public class ControllerIndex {
+	
+	@GetMapping(value = "/", produces = "text/html")
+	public ResponseEntity init() {
+		return new ResponseEntity<>(criaEstruturaHtml(), HttpStatus.OK);
+	}
+	
+	public String criaEstruturaHtml() {
+		StringBuilder html = new StringBuilder();
+		html.append("<h1>API RestFULL Java</h1>");
+		html.append("<h2>Sistema: Monitoria PUC</h2>");
+		
+		html.append("<table style='border: 1px solid black'>");
+		html.append("<tr><th>Funcionalidade</th><th>End-Point</th><th>Type</th><th>Parametros</th></tr>");
+		
+		html.append(criaTable());
+		html.append("</table>");
+		return html.toString();
+	}
+	
+	public String criaTable() {
+		StringBuilder html = new StringBuilder();
+		html.append(criaLinha("Usuário", "http://191.233.244.144:8080/usuario/", "GET", "3"));
+		html.append(criaLinha("Autenticação", "http://191.233.244.144:8080/usuario/", "GET", "3"));	
+		return html.toString();
+	}
+	
+	public String criaLinha(String funcionalidade, String endPoint, String type, String parametros) {
+		StringBuilder html = new StringBuilder();
+		html.append(String.format("<tr><th>%s</th><th>%s</th><th>%s</th><th>%s</th></tr>", funcionalidade, endPoint, type, parametros));
+		return html.toString();
+	}
+	
+}
---
+++ PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/controller/ControllerLogin.java
@@ -0,0 +1,34 @@
+package com.monitoria.puc.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.monitoria.puc.model.UsuarioModel;
+
+@SuppressWarnings({ "unchecked", "rawtypes" })
+@RestController
+@RequestMapping(value = "/login")
+public class ControllerLogin {
+
+	/*@Autowired
+	private RepositoryUsuario usuarioRepository;*/
+
+	@PostMapping(value = "/", produces="application/json")
+	public ResponseEntity<UsuarioModel> login(@RequestBody UsuarioModel usuario) {
+		return  new ResponseEntity(true, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "/autenticacao", produces="application/json")
+	public ResponseEntity<UsuarioModel> autenticacao(@RequestBody UsuarioModel usuario) {
+		/*IMPLEMENTAR METODO DE AUTENTICAÇÃO E SESSION*/
+		return  new ResponseEntity("123456789", HttpStatus.OK);
+	}
+	
+	public static boolean validaUsuarioAutenticado(String codigo) {
+		/*IMPLEMENTAR METODO DE VERIFICAR SE USUARIO ESTA AUTENTICADO*/
+		return codigo.equals("123456") ? true : false;
+	}
+}
