873675fef05c528c21fdf936e070c69d5dbdccb6
Vinicius Vieira <v.vieira.go@gmail.com>
2019-09-29 17:24:53 -0300

    Tipo de alteração: Implementado parte da requisição de login para a API

--- PROJETO/Front-End/_javascript/index.js
+++ PROJETO/Front-End/_javascript/index.js
@@ -2,17 +2,21 @@ $(document).ready(function(){
     $("#btnEntrar").click(function(e){
         var usuario = $('#inputMat').val(); //PEGANDO VALORES DO HTML E INSERINDO EM VARIAVEL JS
 		var senha = $('#inputSenha').val(); //PEGANDO VALORES DO HTML E INSERINDO EM VARIAVEL JS;
+
+
+		var dados = "{'id': '', 'login': '{0}', 'senha': '{1}', 'nome', ''}";
+		dados = dados.replace("{0}", usuario);
+		dados = dados.replace("{1}", senha);
 		
 		alert("Em desenvolvimento, matricula: "+usuario+" senha: "+senha);
 		
-		/*
 		$.ajax({
 		  method: "POST", // TIPO DE REQUISIÇÃO
-		  url: "191.233.244.144/login", // END POINT DA API
-		  data: { user: usuario, password: senha } // JSON COM USUÁRIO E SENHA PEGA DO FORM NO HTML
+		  url: "191.233.244.144/login/", // END POINT DA API
+		  data: dados // JSON COM USUÁRIO E SENHA PEGA DO FORM NO HTML
 		}).done(function(msg){
-				  $("#resultado").html(msg); // MENSAGEM DE RETORNO PELA API EXMP: TRUE, FALSE
-		})*/
+			alert(msg); // MENSAGEM DE RETORNO PELA API EXMP: TRUE, FALSE
+		})
     });
 });
 
