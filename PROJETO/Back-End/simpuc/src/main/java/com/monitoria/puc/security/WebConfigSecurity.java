8adda09935df553af59dc8635ab88632c02d0c07
Vinicius Vieira <v.vieira.go@gmail.com>
2019-10-03 23:15:15 -0300

    Tipo de alteração: Implementado classe de configuração do Spring Security

---
+++ PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/security/WebConfigSecurity.java
@@ -0,0 +1,49 @@
+package com.monitoria.puc.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.monitoria.puc.service.ImplementacaoUserDetailsService;
+
+/*CLASSE DE MAPEAMENTO DE URL/ENDERECOS/END-POINT BLOQUEIA OU LIBERA ACESSO*/
+@Configuration
+@EnableWebSecurity
+public class WebConfigSecurity extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	private ImplementacaoUserDetailsService implementacaoUserDetailsService;
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		/*ATIVANDO A PROTEÇÃO CONTRA USUÁRIO QUE NÃO ESTÃO AUTENTICADOS POR TOKEN*/
+		http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		/*ATIVANDO A RESTRICAO A URL*/
+		.disable().authorizeRequests().antMatchers("/").permitAll()
+		.antMatchers("/index").permitAll()
+		/*URL DE LOGOUT - REDIRECIONA O USUARIO APOS DESLOGAR*/
+		.anyRequest().authenticated().and().logout().logoutSuccessUrl("/index")
+		/*MAPEIA URL DE LOGOUT E INVALIDA USUARIO*/
+		.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+		
+		/*FILTRAR REQUISIÇÕES DE LOGIN PARA AUTENTICACAO*/
+		
+		/*FILTRA DEMAIS REQUISICOES PARA VERIFICAR PRESENCA DO TOKEN JWT NO HEADER HTTP*/
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		/* SERVICE QUE CONSULTA USUARIO NO BANCO */
+		auth.userDetailsService(implementacaoUserDetailsService)
+				/* GERA HASH DA SENHA */
+				.passwordEncoder(new BCryptPasswordEncoder());
+
+	}
+
+}
