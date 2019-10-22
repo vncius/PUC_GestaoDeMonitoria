babbaef16ae11a1d7e543fda3bf96144ed36a65d
Vinicius Vieira <v.vieira.go@gmail.com>
2019-10-21 21:51:13 -0300

    Tipo de alteração: Ajustado padrão cronograma geral e tela de login!

--- PROJETO/Front-End/_javascript/index.js
+++ PROJETO/Front-End/_javascript/index.js
@@ -1,8 +1,7 @@
-﻿$(document).ready(function(){
-	limparLocalStorage();
-
-    $("#btnEntrar").click(function(e){
-        var matricula = $('#inputMat').val();
+﻿$(document).ready(function () {
+	$("#btnEntrar").click(function (e) {
+		limparLocalStorage();
+		var matricula = $('#inputMat').val();
 		var senha = $('#inputSenha').val();
 
 		$.ajax({
@@ -16,10 +15,10 @@
 				"senha": senha
 			}),
 			success: function (result, status, request) {
-				registraTokenEmLocalStorage(result.Authorization, matricula)				
+				registraTokenEmLocalStorage(result.Authorization, matricula)
 			},
 			error: function (request, status, erro) {
-				if(request.status === 403){
+				if (request.status === 403) {
 					alert("Usuário e/ou senha inválidos");
 				} else {
 					console.log("Ocorreu um erro na requisição para o servidor!");
@@ -28,39 +27,34 @@
 		});
 	});
 
-	function limparLocalStorage() {
-		localStorage.setItem("Matricula", "null");
-		localStorage.setItem("Role", "null");
-		localStorage.setItem("Authorization", "null");
-	}
-	
-	function registraTokenEmLocalStorage(token, matricula){
-		$.ajax({
-			method: "GET", // TIPO DE REQUISIÇÃO
-			url: "http://localhost:8080/apimonitoria/usuario/"+matricula, // END POINT DA API
-			headers: {
-				"Authorization": token,
-		   },
-			dataType: "JSON",
-			contentType: "application/json;charset=UTF-8",
-			async: true,
-			success: function (result, status, request) {
-				localStorage.setItem("Matricula", matricula)
-				localStorage.setItem("Role", result.authorities[0].authority)
-				localStorage.setItem("Authorization", token);
-				alert("Usuário autênticado com sucesso!");
-				/*REDIRECIONAR PARA O MENU PRINCIPAL*/
-			},
-			error: function (request, status, erro) {
-				alert("Falha ao consultar dados na API!");
-			}
-		});
-	}
+
 });
 
+function limparLocalStorage() {
+	localStorage.setItem("Matricula", "null");
+	localStorage.setItem("Role", "null");
+	localStorage.setItem("Authorization", "null");
+}
 
-
-
-
-
-
+function registraTokenEmLocalStorage(token, matricula) {
+	$.ajax({
+		method: "GET", // TIPO DE REQUISIÇÃO
+		url: "http://localhost:8080/apimonitoria/usuario/" + matricula, // END POINT DA API
+		headers: {
+			"Authorization": token,
+		},
+		dataType: "JSON",
+		contentType: "application/json;charset=UTF-8",
+		async: true,
+		success: function (result, status, request) {
+			localStorage.setItem("Matricula", matricula);
+			localStorage.setItem("Role", result.authorities[0].authority);
+			localStorage.setItem("Authorization", token);
+			alert("Usuário autênticado com sucesso!");
+			/*REDIRECIONAR PARA O MENU PRINCIPAL*/
+		},
+		error: function (request, status, erro) {
+			alert("Falha ao consultar dados na API!");
+		}
+	});
+}
