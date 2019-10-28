4cc9ff1c91f17fca740f3ce373eb1619fbec0f8f
juliofti <43100202+juliofti@users.noreply.github.com>
2019-10-27 23:05:09 -0300

    Adicionado controller,repository e service de Curso

---
+++ PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/service/CursoService.java
@@ -0,0 +1,29 @@
+package com.monitoria.puc.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.monitoria.puc.exception.NotFoundException;
+import com.monitoria.puc.model.ModelCurso;
+import com.monitoria.puc.repository.RepositoryCurso;
+
+@Service
+public class CursoService {
+	
+	@Autowired
+	private RepositoryCurso repositoryCurso;
+	
+	public List<ModelCurso> listAll(){
+		List<ModelCurso> listaDeCursos = repositoryCurso.findAll();
+		return listaDeCursos;
+	}
+	
+	public ModelCurso findById(Long id) {
+		Optional<ModelCurso> result = repositoryCurso.findById(id);
+		return result.orElseThrow(()-> new NotFoundException("Não existe curso com o id: = " + id));
+	}
+
+}
