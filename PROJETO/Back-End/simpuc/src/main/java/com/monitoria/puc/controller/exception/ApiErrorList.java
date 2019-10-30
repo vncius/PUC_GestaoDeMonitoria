e885e70569710d8fa830dcc269f0cd4c4e0419c3
Vinicius Vieira <v.vieira.go@gmail.com>
2019-10-30 18:32:20 -0300

    Tipo de alteração: Ajustado inconsitências da biblioteca lombok, removida também do POM

--- PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/controller/exception/ApiErrorList.java
+++ PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/controller/exception/ApiErrorList.java
@@ -3,10 +3,6 @@ package com.monitoria.puc.controller.exception;
 import java.util.Date;
 import java.util.List;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
 public class ApiErrorList extends ApiError {
 
 	/**
@@ -14,6 +10,14 @@ public class ApiErrorList extends ApiError {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public List<String> getErrors() {
+		return errors;
+	}
+
+	public void setErrors(List<String> errors) {
+		this.errors = errors;
+	}
+
 	private List<String> errors;
 	
 	public ApiErrorList(int code, String msg, Date date, List<String> errors) {
