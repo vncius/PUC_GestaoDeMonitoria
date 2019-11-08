e3756c769d77114c8505f4b6a53cb1e1969101f0
Vinicius Vieira <v.vieira.go@gmail.com>
2019-11-08 18:18:43 -0300

    Tipo de alteração: Commit do repository do requisito de inscrição

--- PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/repository/RepositoryIncricaoMonitoria.java
+++ PROJETO/Back-End/simpuc/src/main/java/com/monitoria/puc/repository/RepositoryIncricaoMonitoria.java
@@ -2,6 +2,7 @@ package com.monitoria.puc.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
 import com.monitoria.puc.model.ModelInscricaoMonitoria;
 
 @Repository
