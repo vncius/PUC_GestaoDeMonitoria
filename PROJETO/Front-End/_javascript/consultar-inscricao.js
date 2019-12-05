86cf2fc8e0c04cb816384e69bd99a2d9a4d38349
Vinicius Vieira Abreu <31490903+vncius@users.noreply.github.com>
2019-12-05 19:23:04 -0300

    Tipo de alteração: Alterado forma de chamar link de download na parte client

--- PROJETO/Front-End/_javascript/consultar-inscricao.js
+++ PROJETO/Front-End/_javascript/consultar-inscricao.js
@@ -125,8 +125,6 @@ function buscarInscricoesPorFiltro(filtrarPorCurso, filtrarPorStatus, filtrarPor
 // ------------------------------------------------------------------ FUNÇÕES AUXILIARES
 function preencherTableDeInscricoes(inscricoes) {
     $('.table tbody tr').remove();
-
-    
     var linha = "";
     inscricoes.forEach(inscricao => {
         var linkDownload = obterUrlDaAPI('/fichaInscricao/downloadAnexo/');
@@ -137,12 +135,41 @@ function preencherTableDeInscricoes(inscricoes) {
         linha = linha.concat(`<td>${inscricao.matricula}</td>`);
         linha = linha.concat(`<td>${inscricao.nome}</td>`);
         linha = linha.concat(`<td>${inscricao.cursos.descricao}</td>`);
-        linha = linha.concat(`<td><a href="${linkDownload}">Download</a></td>`);
+        linha = linha.concat(`<td><a href="#" onclick="downloadAnexo('${inscricao.matricula}');">Download</a></td>`);
         linha = linha.concat(`<td>${inscricao.statusIncricao}</td>`);
         linha = linha.concat("</tr>");
         inserirLinhaTable(linha);
         linha = "";
     });
+}
+
+function downloadAnexo(matricula) {
+    var fileURL = obterUrlDaAPI('/fichaInscricao/downloadAnexo/'+matricula);
+
+    // for non-IE
+    if (!window.ActiveXObject) {
+        var save = document.createElement('a');
+        save.href = fileURL;
+        save.target = '_blank';
+        save.download = "anexo.pdf" || 'unknown';
+
+        var evt = new MouseEvent('click', {
+            'view': window,
+            'bubbles': true,
+            'cancelable': false
+        });
+        save.dispatchEvent(evt);
+
+        (window.URL || window.webkitURL).revokeObjectURL(save.href);
+    }
+
+    // for IE < 11
+    else if ( !! window.ActiveXObject && document.execCommand)     {
+        var _window = window.open(fileURL, '_blank');
+        _window.document.close();
+        _window.document.execCommand('SaveAs', true, fileName || fileURL)
+        _window.close();
+    }
 }
 
 function preencherCamposBusca(inscricoes) {
